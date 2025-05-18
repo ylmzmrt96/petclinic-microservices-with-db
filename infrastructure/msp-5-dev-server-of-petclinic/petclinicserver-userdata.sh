75200c775d22c5dc82802e28297ad75bc44a8480
necatisekmenoglu <ylmzmrt96@gmail.com>
2025-05-18 02:26:47 +0000

    added terraform files for dev server

---
+++ infrastructure/msp-5-dev-server-of-petclinic/petclinicserver-userdata.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+dnf update -y
+hostnamectl set-hostname petclinic-dev-server
+dnf install docker -y
+systemctl start docker
+systemctl enable docker
+usermod -a -G docker ec2-user
+curl -SL https://github.com/docker/compose/releases/download/v2.29.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+dnf install git -y
+dnf install java-11-amazon-corretto -y
